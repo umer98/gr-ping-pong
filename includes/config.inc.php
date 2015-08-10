f0aab8feab77b99dc90209451f968cbf876adf28
Umer Khan <umer.khan@grandrounds.com>
2015-08-09 21:41:38 -0700

    Bug fix: database connection issue

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -30,7 +30,6 @@ $db = substr($url["path"], 1);
 
 // Make the connection:
 $dbc = mysqli_connect();
-if (!$dbc) { die("mysqli_init failed"); }
 
 mysqli_ssl_set($dbc, "../other/bb8c390b34cb99-key.pem", "../other/bb8c390b34cb99-cert.pem", "../other/cleardb-ca.pem",NULL,NULL);
 
